7c5d54ec6ef2a1d409dc09b1d83f5f7d661e1373
Artur Shchekach <arturshchekatch@yandex.ru>
2025-03-25 06:49:57 +1000

    feat: add packages

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 import handlebars from 'vite-plugin-handlebars';
-import autoprefixer from 'autoprefixer'
-
+import autoprefixer from 'autoprefixer';
+import mediaquery from 'postcss-combine-media-query';
+import cssnano from 'cssnano';
 
 export default defineConfig({
-  plugins: [handlebars()],
+  server: {
+    port: 3005,
+    watch: { usePolling: true },
+  },
+  preview: {
+    port: 3000,
+  },
+  plugins: [
+      handlebars()
+  ],
   css: {
     postcss: {
       plugins: [
-        autoprefixer({}) // add options if needed
+        autoprefixer({}), // add options if needed,
+        mediaquery(),
+        cssnano()
       ],
     }
-  }
-})
+  },
+});
