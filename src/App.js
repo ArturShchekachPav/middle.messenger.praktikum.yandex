e1e36a7720b599dfa42da602b37d0a7878c0cae0
Artur Shchekach <arturshchekatch@yandex.ru>
2025-03-26 07:43:54 +1000

    feat: add pages

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Handlebars from 'handlebars';
+import * as Pages from './pages';
+
+export default class App {
+    constructor() {
+        this.state = '/',
+        this.appElement = document.getElementById('app');
+    }
+
+    render() {
+        let template;
+
+    }
+}
