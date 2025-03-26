c12ffae39a006c0742bb816212407f378bb8e0b9
Artur Shchekach <arturshchekatch@yandex.ru>
2025-03-27 07:40:39 +1000

    feat: add pages

--- src/App.js
+++ src/App.js
@@ -2,13 +2,43 @@ import Handlebars from 'handlebars';
 import * as Pages from './pages';
 
 export default class App {
-    constructor() {
-        this.state = '/',
+    constructor(currentPage) {
+        this.currentPage = currentPage,
         this.appElement = document.getElementById('app');
     }
 
     render() {
         let template;
+        switch (this.currentPage) {
+            case '/':
+                template = Handlebars.compile(Pages.MessangerPage);
+                this.appElement.innerHTML = template();
+                break;
+            case '/500':
+                template = Handlebars.compile(Pages.ServerErrorPage);
+                this.appElement.innerHTML = template();
+                break;
+            case '/404':
+                template = Handlebars.compile(Pages.NotFoundPage);
+                this.appElement.innerHTML = template();
+                break;
+            case '/profile':
+                template = Handlebars.compile(Pages.Profile);
+                this.appElement.innerHTML = template();
+                break;
+            case '/sing-up':
+                template = Handlebars.compile(Pages.RegistrationPage);
+                this.appElement.innerHTML = template();
+                break;
+            case '/sing-in':
+                template = Handlebars.compile(Pages.LoginPage);
+                this.appElement.innerHTML = template();
+                break;
+        }
+    }
 
+    changePage(page) {
+        this.currentPage = page;
+        this.render();
     }
 }
---
+++ src/pages/messanger/index.js
@@ -0,0 +1,1 @@
+export {default as MessangerPage} from './messanger.hbs?raw';
