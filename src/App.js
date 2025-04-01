0866c0292b16590b760a727715168f117415cb00
Artur Shchekach <arturshchekatch@yandex.ru>
2025-04-02 06:37:42 +1000

    fead: add popups

--- src/App.js
+++ src/App.js
@@ -10,11 +10,17 @@ import {
 import './App.scss';
 import {
     Link,
-    ErrorPage
+    ErrorPage,
+    Message,
+    ChatPreview,
+    Button
 } from './components';
 
 Handlebars.registerPartial('Link', Link);
 Handlebars.registerPartial('ErrorPage', ErrorPage);
+Handlebars.registerPartial('Message', Message);
+Handlebars.registerPartial('ChatPreview', ChatPreview);
+Handlebars.registerPartial('Button', Button);
 
 export default class App {
     constructor(currentPage) {
@@ -28,31 +34,116 @@ export default class App {
             case '/':
                 template = Handlebars.compile(MessangerPage);
                 this.appElement.innerHTML = template();
+
+                this.setRoutingLinks();
+
+                this.popupAddFile = document.querySelector('#popup-add-file');
+
+                this.buttonAddFile = document.querySelector('.chat-window__file-button');
+
+                this.buttonAddFile.addEventListener('click', () => {
+                    this.popupAddFile.classList.add('popup_open');
+                });
+
+                this.popupAddFile.addEventListener('click', e => {
+                    if(e.target === e.currentTarget) {
+                        this.popupAddFile.classList.remove('popup_open');
+                    }
+                });
                 break;
             case '/500':
                 template = Handlebars.compile(ServerErrorPage);
                 this.appElement.innerHTML = template();
+
+                this.setRoutingLinks();
                 break;
             case '/profile':
                 template = Handlebars.compile(ProfilePage);
                 this.appElement.innerHTML = template();
+
+                this.setRoutingLinks();
+
+                this.avatarEditButton = document.querySelector('#edit-avatar-button');
+                this.commonDataEditButton = document.querySelector('#edit-common-data-button');
+                this.changePasswordButton = document.querySelector('#edit-password-button');
+                this.singOutButton = document.querySelector('#sing-out-button');
+
+                this.popupAvatarEdit = document.querySelector('#popup-avatar-edit');
+
+                this.avatarEditButton.addEventListener('click', () => {
+                    this.popupAvatarEdit.classList.add('popup_open');
+                });
+                this.commonDataEditButton.addEventListener('click', () => {
+
+                });
+                this.changePasswordButton.addEventListener('click', () => {
+
+                });
+                this.singOutButton.addEventListener('click', () => {
+                    this.changePage('/sing-in');
+                });
+
+                this.popupAvatarEdit.addEventListener('click', e => {
+                    if(e.target === e.currentTarget) {
+                        this.popupAvatarEdit.classList.remove('popup_open');
+                    }
+                });
+
                 break;
             case '/sing-up':
                 template = Handlebars.compile(RegistrationPage);
                 this.appElement.innerHTML = template();
+
+                this.setRoutingLinks();
+
+                this.registration = document.forms.registration;
+
+                this.registration.addEventListener('submit', e => {
+                    e.preventDefault();
+
+                    this.changePage('/sing-in');
+                });
                 break;
             case '/sing-in':
                 template = Handlebars.compile(LoginPage);
                 this.appElement.innerHTML = template();
+
+                this.setRoutingLinks();
+
+                this.loginForm = document.forms.login;
+
+                this.loginForm.addEventListener('submit', e => {
+                    e.preventDefault();
+
+                    this.changePage('/');
+                });
                 break;
             default:
                 template = Handlebars.compile(NotFoundPage);
                 this.appElement.innerHTML = template();
+
+                this.setRoutingLinks();
         }
     }
 
     changePage(page) {
         this.currentPage = page;
+        history.pushState(null, null, page);
+
         this.render();
+    }
+
+    setRoutingLinks() {
+        const routingLinks = Array.from(document.querySelectorAll('a[data-page]'));
+
+        routingLinks.forEach(routingLink => {
+            routingLink.addEventListener('click', (e) => {
+                e.preventDefault();
+
+                const page = routingLink.dataset.page;
+
+                this.changePage(page);
+            });
+        });
     }
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,5 @@
 export { Link } from './Link';
 export { ErrorPage } from './ErrorPage';
+export { Message } from './Message';
+export { ChatPreview } from './ChatPreview';
+export { Button } from './Button';
