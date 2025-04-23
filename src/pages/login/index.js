9d7aaf4e26a5d4ba0acca5d6e33dd1b8a98aca66
Artur Shchekach <arturshchekatch@yandex.ru>
2025-04-24 07:01:24 +1000

    feat: add framework

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,1 +1,16 @@
-export { default as LoginPage } from './login.hbs?raw';
+import Block from '../../framework/Block.js';
+import { default as layout } from './login.hbs?raw';
+
+export class LoginPage extends Block {
+	constructor() {
+		super({
+			Field: new Field({}),
+			Button: new Button({}),
+			Link: new Link({})
+		});
+	}
+
+	render() {
+		return layout;
+	}
+}
