b0f6f0ac39bd0ed7f2a3afc2754a3d2a45bed64e
Artur Shchekach <arturshchekatch@yandex.ru>
2025-06-26 19:40:46 +1000

    add jest config to eslintignore

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,6 @@ import { createDefaultPreset } from "ts-jest";
 
 const tsJestTransformCfg = createDefaultPreset().transform;
 
-/** @type {import("jest").Config} **/
 export default {
 	testEnvironment: "node",
 	transform: {
